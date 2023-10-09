be35e871db9028f8958289c15122a386f23a3e61
Tarek994 <ghali.tarek.123@hotmail.com>
2023-10-09 05:28:31 +0300

    counter fun

--- simpleLogec.tsx
+++ simpleLogec.tsx
@@ -1,3 +1,5 @@
+// Simple Javascript fun
+
 function createHelloWorld() {
     
     return function(...args: any[]): string {
@@ -9,3 +11,23 @@ function createHelloWorld() {
 * const f = createHelloWorld();
 * f(); // "Hello World"
 */
+
+/* Counter */
+
+function createCounter(n: number): () => number {
+    let count = n;
+
+     function counter() {
+		return count++;
+    }
+    return counter;
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
