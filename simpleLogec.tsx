66bbfd46d3a773cb6cd859c6adc60ce9f80dc786
Tarek994 <ghali.tarek.123@hotmail.com>
2023-10-10 23:52:03 +0300

    to be or not to be function

--- simpleLogec.tsx
+++ simpleLogec.tsx
@@ -31,3 +31,34 @@ function createCounter(n: number): () => number {
  * counter() // 12
  */
 
+
+   /* 2704. To Be Or Not To Be */
+   
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (expectedVal: any) => {
+            if (val === expectedVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: (expectedVal: any) => {
+            if (val !== expectedVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        },
+    };
+}
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
